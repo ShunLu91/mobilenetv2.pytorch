f6375af1145bffb03e8a6e90fe212c9d3c51c7a0
lushun_ict <470651748@qq.com>
2020-05-30 08:52:41 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+export CUDA_VISIBLE_DEVICES=2,7 && nohup python -u imagenet.py -a mobilenetv2 \
+    -b 256 \
+    -d /dataset/imagenet \
+    --epochs 150 \
+    --lr-decay cos \
+    --lr 0.05 \
+    --wd 4e-5 \
+    -c ./snapshots \
+    --width-mult 1.0 \
+    --input-size 224 \
+    -j 16 > logdir/a0.log 2>&1 &
+tail -f logdir/a0.log
+~
+~
