aa47e7545492d16599daf643aa2079cfefcc55fe
lushun_mac <470651748@qq.com>
2020-06-04 22:10:59 +0800

    3rd commit occupy.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 source activate torch1.0
-export CUDA_VISIBLE_DEVICES=1,5 && nohup python -u imagenet.py -a mobilenetv2 \
+export CUDA_VISIBLE_DEVICES=4,5 && nohup python -u imagenet.py -a mobilenetv2 \
     -b 256 \
     -d /dataset/imagenet \
     --epochs 150 \
